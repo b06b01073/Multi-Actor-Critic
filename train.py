4da8f22b5de050a4d9c8891e362898ddbf70da28
Bella_Shiu <wasbella1017lg@gmail.com>
2023-05-09 16:40:20 +0800

    add file

--- train.py
+++ train.py
@@ -3,22 +3,37 @@ import matplotlib.pyplot as plt
 
 from component_agent import ComponentAgent
 import env
+from memory import EpisodicMemory
 
 def train(args):
     agent = ComponentAgent(args.asset)
     market = env.make(csv_path=args.data_path, start=args.start, end=args.end)
 
-    state, _ = market.reset()
+    memory = EpisodicMemory(capacity=args.rmsize, max_train_traj_len=args.exp_traj_len,
+                            window_length=args.window_length)
     earnings = []
 
-    while True:
-        action, invested_asset = agent.take_action(state)
-        state, reward, terminated, earning,_ = market.step(action, invested_asset)
+    for i in range(args.epoch):
+        obs = market.reset()
+        agent.reset_noise()
+        total_reward = 0
 
+        for _ in range(args.max_iter):
+            action, invested_asset = agent.take_action(obs)
+            next_obs, earning, terminated, _ = market.step(action, invested_asset)
 
-        earnings.append(earning - args.asset)
-        agent.update_asset(earning)
-        agent.learn()
+            # memory.append(action_bc, state0, action, reward, done)
+            memory.append(obs, action, earning, next_obs, terminated)
+            obs = next_obs
+            experiences = memory.sample(args.batch_size)
+
+            earnings.append(earning - args.asset)
+            agent.update_asset(earning)
+
+            agent.learn(experiences)
+            agent.soft_update()
+
+            total_reward += earning
 
         if terminated:
             break
